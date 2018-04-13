9975a501cbb5f9dcbedf3a12edc7e72be510e695
Manos Kouvarakis <manoskouvarakis@gmail.com>
2018-04-13 18:44:17 +0300

    Add project files.

---
+++ BitcoinCrawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BitcoinCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
