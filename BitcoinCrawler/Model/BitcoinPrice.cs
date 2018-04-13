9d10685a0be84cb1bf089441713331c99bdce628
Manos Kouvarakis <manoskouvarakis@gmail.com>
2018-04-14 01:25:30 +0300

    Implement basic bitcoin price model

---
+++ BitcoinCrawler/Model/BitcoinPrice.cs
@@ -0,0 +1,24 @@
+﻿using BitcoinCrawler.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitcoinCrawler.Model
+{
+	public class BitcoinPrice
+	{
+		public enum OriginType
+		{
+			Gdax,
+			Bitstamp,
+		}
+
+		public long Timestamp { get; set; }
+
+		public decimal Value { get; set; }
+
+		public OriginType Origin { get; set; }
+	}
+}
